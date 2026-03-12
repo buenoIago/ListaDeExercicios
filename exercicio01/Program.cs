d6df141abe4b5299a9ca438482a9940c5ddb6ad4
buenoIago <iagocedup@gmail.com>
2026-03-12 14:33:39 -0300

    adicionado exercicio 2

--- exercicio01/Program.cs
+++ exercicio01/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("");
+﻿//1. Crie um programa para calcular o volume de uma caixa retangular
+//multiplique o comprimento x largura x altura (ou profundidade)
+
+Console.WriteLine("Digite o comprimento da caixa: ");
+decimal comprimento = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a largura da caixa: ");
+decimal largura = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a altura da caixa: ");
+decimal altura = Convert.ToInt32(Console.ReadLine());
+
+decimal volume = comprimento * largura * altura;
+
+Console.WriteLine($"O volume da caixa retangular é {volume}.");
---
+++ exercicio02/Program.cs
@@ -0,0 +1,15 @@
+﻿//2. Crie um programa para calcular o volume de um cilindro
+// V = PI . r² . h
+
+Console.WriteLine("Digite o raio do cilindro: ");
+double raio = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a altura do cilindro: ");
+double altura = Convert.ToInt32(Console.ReadLine());
+
+//double areaBase = Math.PI * Math.Pow(raio, 2);
+
+double areaBase = 3.14 * raio * raio;
+double volume = areaBase * altura;
+
+Console.WriteLine($"O volume do cilindro é: {volume} cm³");
