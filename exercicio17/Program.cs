b6650e7ae78cb40dfb054ff449cfc15b85448c4d
buenoIago <iagocedup@gmail.com>
2026-03-13 15:02:59 -0300

    adicionado exercicio 17 e 18

---
+++ exercicio17/Program.cs
@@ -0,0 +1,25 @@
+﻿//Faça um algoritmo que leia dois valores inteiros A e B, se os valores 
+//forem iguais deverá se somar os dois, caso contrário multiplique A por B.
+//Ao final de qualquer um dos cálculos deve-se atribuir o resultado para 
+//uma variável C e mostrar seu conteúdo na tela.
+
+Console.Write("Informe um número: ");
+double A = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe um segundo número: ");
+double B = Convert.ToDouble(Console.ReadLine());
+
+double C;
+
+if (A == B)
+{
+    C = A + B;
+}
+else
+{
+    C = A * B;
+}
+
+Console.WriteLine($"Resultado da variável C: {C}");
+
+Console.ReadLine();
---
+++ exercicio18/Program.cs
@@ -0,0 +1,12 @@
+﻿//Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+
+Console.WriteLine("Números de 100 a 200:"); 
+
+while (true)
+{
+    for (int i = 100; i <= 200; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    Console.ReadLine();
+}
